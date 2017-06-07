428e3dab4ef7783f023b7ca0a31680ef0f808128
Paul Alesius <paul@unnservice.com>
2017-06-07 12:46:44 +0200

    Remove abstract activator

--- com.unnsvc.erhena.feature.ide/com.unnsvc.erhena.core/src/main/java/com/unnsvc/erhena/core/Activator.java
+++ com.unnsvc.erhena.feature.ide/com.unnsvc.erhena.core/src/main/java/com/unnsvc/erhena/core/Activator.java
@@ -1,31 +1,55 @@
 
 package com.unnsvc.erhena.core;
 
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 import com.unnsvc.erhena.core.builder.ModuleResourceChangeListener;
 
-/**
- * The activator class controls the plug-in life cycle
- */
-public class Activator extends AbstractActivator {
+public class Activator extends AbstractUIPlugin {
 
+	public static final String PLUGIN_ID = "com.unnsvc.erhena.core";
+	private static BundleActivator plugin;
+	private IWorkspace workspace;
 	private ModuleResourceChangeListener listener;
 
 	public Activator() {
 
 	}
 
-	@Override
-	public void startBundle(BundleContext context) throws Exception {
+	public void start(BundleContext context) throws Exception {
 
+		super.start(context);
+		plugin = this;
+		workspace = ResourcesPlugin.getWorkspace();
 		listener = new ModuleResourceChangeListener();
 		getWorkspace().addResourceChangeListener(listener);
 	}
 
-	@Override
-	public void stopBundle(BundleContext context) throws Exception {
+	public void stop(BundleContext context) throws Exception {
 
 		getWorkspace().removeResourceChangeListener(listener);
+		plugin = null;
+		workspace = null;
+		super.stop(context);
+	}
+
+	public static BundleActivator getDefault() {
+
+		return plugin;
+	}
+
+	public static ImageDescriptor getImageDescriptor(String path) {
+
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+
+	public IWorkspace getWorkspace() {
+
+		return workspace;
 	}
 }
